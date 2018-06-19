8e7dfcd990b42ba359930201ca1378448fb5f3d2
Matt Greff <greff.matt@icloud.com>
2018-06-19 16:07:41 +0000

    creating stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -6,14 +6,21 @@ puts "----------"
 
 # Your code goes below here ...
 
-CREATE TABLE products (
-   id int(11) NOT NULL auto_increment,
-   name varchar(255),
-   PRIMARY KEY  (id)
-);
+class Outlet
 
+    def initialize(name, annual_revenue, mens_apparel, womens_apparel)
+        @store = Store.create(
+            name:           name, 
+            annual_revenue: annual_revenue,
+            mens_apparel:   mens_apparel,
+            womens_apparel: womens_apparel
+        )
+    end
+    attr_accessor :store
 
+end
 
-#Burnaby (annual_revenue of 300000, carries men's and women's apparel)
-#Richmond (annual_revenue of 1260000 carries women's apparel only)
-#Gastown (annual_revenue of 190000 carries men's apparel only)
+#                     ||name     ||revenue||mens ||womens||
+burnaby  = Outlet.new( 'Burnaby' , 300000 , true , false  )
+richmond = Outlet.new( 'Richmond', 1260000, false, true   )
+gastown  = Outlet.new( 'Gastown' , 190000 , true , false  )
