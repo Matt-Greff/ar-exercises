4133a45c8069660097e4359fd27fa27de7d8e819
Matt Greff <greff.matt@icloud.com>
2018-06-19 16:46:41 +0000

    made method to update store name in database

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -10,10 +10,10 @@ class Outlet
 
     def initialize(name, annual_revenue, mens_apparel, womens_apparel)
         @store = Store.create(
-            name:           name, 
-            annual_revenue: annual_revenue,
-            mens_apparel:   mens_apparel,
-            womens_apparel: womens_apparel
+            name:            name, 
+            annual_revenue:  annual_revenue,
+            mens_apparel:    mens_apparel,
+            womens_apparel:  womens_apparel
         )
     end
     attr_accessor :store
@@ -24,3 +24,7 @@ end
 burnaby  = Outlet.new( 'Burnaby' , 300000 , true , false  )
 richmond = Outlet.new( 'Richmond', 1260000, false, true   )
 gastown  = Outlet.new( 'Gastown' , 190000 , true , false  )
+
+#Burnaby  (annual_revenue of 300000, carries men's and women's apparel)
+#Richmond (annual_revenue of 1260000 carries women's apparel only)
+#Gastown  (annual_revenue of 190000 carries men's apparel only)
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,6 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@burnaby  = Store.find_by(id: 1)
+@richmond = Store.find_by(id: 2)
+@burnaby.update(name: "Burbs")
